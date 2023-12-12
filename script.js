079970add9537c971b28f56239d9a351fa6761c0
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-12 12:25:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function result(input1, input2) =>{
+	let value1 = input1*2;
+	let value2 = input*1;
+	let result = value1 + value2;
+	console.log("The final result is: ${result}");
+}
